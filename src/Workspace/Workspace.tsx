74b4aa54a49b122d851e68b039ebbc90a1e7329e
Manohar123261 <121283980+Manohar123261@users.noreply.github.com>
2024-07-12 21:43:46 +0530

    basic structure of code environment is done

---
+++ src/Workspace/Workspace.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+type workspaceProps = {
+    
+};
+
+const workspace:React.FC<workspaceProps> = () => {
+    
+    return <div>Have a good coding</div>
+}
+export default workspace;
